3d0fc17332965980a74391835d2e73446ffb4992
aryasaja <mushallahku19@gmail.com>
2023-11-06 09:48:30 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['prim'] = array ('1'=>'prim!103.50.10.1','prim@|@aplikasi','prim#|#nJaepJqpmZ9yeZebo5JkaGJq','prim%pri','prim^mik','prim&Rp','prim*','prim(','prim)','prim=30','prim@!@disable','prim#!#');
